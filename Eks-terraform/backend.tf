de214aa1ace6edd6139fc9b6407144e120c97c22
Satya Krishna Ranga Rao <108000447+satya19977@users.noreply.github.com>
2024-03-01 09:15:38 -0500

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "jenkinstetriscicd" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
+    key    = "Jenkins-EKS/terraform.tfstate"
     region = "us-east-1"
   }
 }
