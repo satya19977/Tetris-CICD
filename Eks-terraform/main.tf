9e261d8cff19ee85ac7c3082d1cb6b775e1eccd8
Satya Krishna Ranga Rao <108000447+satya19977@users.noreply.github.com>
2024-03-01 14:39:51 -0500

    Update main.tf

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -41,7 +41,7 @@ resource "aws_eks_cluster" "example" {
     subnet_ids = data.aws_subnets.public.ids
   }
   # Specify desired availability zones
-  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d","us-east-1d"]
+  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1f"]
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
   # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
