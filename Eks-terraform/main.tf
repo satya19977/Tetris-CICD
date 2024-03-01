cf4698ece6cdfb79812c23f17a50e5ce1cbfdcd2
Satya Krishna Ranga Rao <108000447+satya19977@users.noreply.github.com>
2024-03-01 16:49:04 -0500

    Update main.tf

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -46,6 +46,7 @@ resource "aws_eks_cluster" "example" {
 
   vpc_config {
     subnet_ids = data.aws_subnets.public.ids
+    availability_zones = data.aws_availability_zones.example.names
   }
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
   # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
